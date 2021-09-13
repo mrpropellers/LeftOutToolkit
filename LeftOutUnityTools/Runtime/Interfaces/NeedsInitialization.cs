dfdf05582d54fa4efbea97b30d6a69ea3c437a60
Devin Miller <devook@gmail.com>
2021-09-12 19:33:38 -0700

    Initial commit

---
+++ LeftOutUnityTools/Runtime/Interfaces/NeedsInitialization.cs
@@ -0,0 +1,8 @@
+﻿namespace LeftOut.Runtime.Interfaces
+{
+    public interface INeedsInitialization<in T>
+    {
+        bool IsInitialized { get; }
+        bool TryInitialize(T initializer);
+    }
+}
---
+++ LeftOutUnityTools/Runtime/ObjectPool.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LeftOut.Runtime.Interfaces;
+using UnityEngine;
+#if UNITY_EDITOR
+using LeftOut.Editor;
+#endif
+
+namespace LeftOut.Runtime
+{
+    // TODO: Add subclass for MonoBehaviour pool
+    // Derives from ScriptableObject so we can access the OnDestroy event -- this might not be necessary...
+    public class ObjectPool 
+    {
+        protected GameObject m_Original;
+        Queue<GameObject> m_PoolInactive;
+        HashSet<GameObject> m_TrackedInstances;
+        
+        public ObjectPool(GameObject goToPool)
+        {
+            m_Original = goToPool;
+            m_PoolInactive = new Queue<GameObject>();
+            m_TrackedInstances = new HashSet<GameObject>();
+        }
+
+        public GameObject GetOrInstantiate(Transform origin = null, string newName = "", bool shouldAttachToOrigin = false)
+        {
+            GameObject go;
+            if (m_PoolInactive.Any())
+            {
+                go = m_PoolInactive.Dequeue();
+            }
+            else
+            {
+                go = GameObject.Instantiate(m_Original, origin);
+                m_TrackedInstances.Add(go);
+            }
+
+            if (!newName.Equals(""))
+            {
+                go.name = newName;
+            }
+
+            go.transform.SetParent(shouldAttachToOrigin ? origin : null);
+
+            go.SetActive(true);
+            return go;
+        }
+
+        public void ReturnToPool(GameObject go)
+        {
+            if (!m_TrackedInstances.Contains(go))
+            {
+                throw new InvalidOperationException(
+                    $"Can't return {go.name} to pool for {m_Original.name} because it was not created by this pool.");
+            }
+
+            go.SetActive(false);
+            m_PoolInactive.Enqueue(go);
+        }
+    }
+
+    public class PrefabPool : ObjectPool
+    {
+        public GameObject Prefab => m_Original;
+        
+        public PrefabPool(GameObject prefab) : base(prefab)
+        {
+#if UNITY_EDITOR
+            if (prefab != null && !EditorHelpers.IsAPrefab(prefab))
+            {
+                Debug.LogError($"Can't create a prefab pool from {prefab.name} because it's not a prefab.");
+                m_Original = null;
+            }
+#endif
+        }
+
+    }
+}
