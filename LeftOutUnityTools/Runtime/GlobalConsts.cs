610898d19c304b67c7d209a938f1e4707162808e
Devin Miller <mrpropellers@users.noreply.github.com>
2022-08-16 12:12:51 -0700

    Add default Unity tags to GlobalConsts

--- LeftOutUnityTools/Runtime/GlobalConsts.cs
+++ LeftOutUnityTools/Runtime/GlobalConsts.cs
@@ -1,9 +1,20 @@
-﻿namespace LeftOut.Runtime.GlobalConsts
+﻿namespace LeftOut.GlobalConsts
 {
     public static class ShaderProperty
     {
         // TODO: Make this renderer agnostic with pre-processor defines
         public const string MainColor = "_Color";
         public const string EmissiveColor = "_EmissionColor";
+    }
+
+    public static class Tags
+    {
+        public const string Untagged = "Untagged";
+        public const string Respawn = "Respawn";
+        public const string Finish = "Finish";
+        public const string EditorOnly = "EditorOnly";
+        public const string MainCamera = "MainCamera";
+        public const string Player = "Player";
+        public const string GameController = "GameController";
     }
 }
